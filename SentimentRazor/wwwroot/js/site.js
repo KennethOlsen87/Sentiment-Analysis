8afd949e35142a1269f13d8eb43b996955bc05e5
Kenneth <kennetholsen87@gmail.com>
2020-01-24 13:20:04 +0100

    Dynamically update the position of the marker

--- SentimentRazor/wwwroot/js/site.js
+++ SentimentRazor/wwwroot/js/site.js
@@ -1,7 +1,16 @@
-﻿function getSentiment(userInput) {
+﻿// make a GET HTTP request with the user input to the 
+//OnGetAnalyzeSentiment handler.
+function getSentiment(userInput) {
     return fetch(`Index?handler=AnalyzeSentiment&text=${userInput}`)
         .then((response) => {
             return response.text();
         })
 }
 
+
+// Dynamically update the position of the marker on
+// the web page as sentiment is predicted.
+function updateMarker(markerPosition, sentiment) {
+    $("#markerPosition").attr("style", `left:${markerPosition}%`);
+    $("#markerValue").text(sentiment);
+}
